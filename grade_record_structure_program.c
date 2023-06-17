9c0af660455531be5fa1bfcbc7c52d5979dfc38a
ro7uello <janrowellarouello@gmail.com>
2023-06-17 12:05:32 +0800

    given task

---
+++ grade_record_structure_program.c
@@ -0,0 +1,28 @@
+/*Quiz in Programming 2
+
+Write a program that will input the following information:
+1. Name
+2. Midterm grade
+3. Final grade
+
+The program will accept the information above then compute for the average grade.
+
+4. Average grade (ave. grade = (midterm grade + final grade)/2)
+
+After each record, the program will ask the user if another record will be entered. If the user opted to input another record, it will ask the user to input the information above and compute for the average grade. If the user opted not to input another record, the program will terminate and display all the records inputted by the user.
+
+Output requirement
+Name	Midterm Grade	Final Grade	Ave. Grade
+Pat	1.50		2.00		1.75
+Mat	2.00		3.00		2.50
+Lev	1.00		1.50		1.25
+Curl	1.00		1.00		1.00
+Jet	2.25		1.75		2.00
+
+Input at least 5 records.
+
+Use a structure for this program.
+
+Email the source code and the screenshots of the output.
+
+Deadline of submission: 11:59pm of June 16, 2023*/
